a9e73f8e11e76ca2a14b633275e02ec042411ac0
Graylin Kim <graylin.kim@gmail.com>
2014-01-27 15:29:05 -0500

    Add example configuration file.

---
+++ starbowmodweb/example.config.py
@@ -0,0 +1,33 @@
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'super_secret_key!!'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '',
+        'NAME': 'starbow_website',
+        'USER': 'root',
+        'PASSWORD': '',
+    }
+}
+
+# Static files
+# https://docs.djangoproject.com/en/1.6/howto/static-files/#deployment
+MEDIA_ROOT = BASE_DIR+"/media/"
+STATIC_ROOT = BASE_DIR+"/static/"
+
+# This is used by BROWSERID to verify login requests. Must match running domain
+SITE_URL = 'http://localhost:8000'
