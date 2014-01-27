5dccda040e9244319d6fcbe78fd9d7e0179553ad
Graylin Kim <graylin.kim@gmail.com>
2014-01-27 15:27:17 -0500

    Limit main site url match.

--- starbowmodweb/site/urls.py
+++ starbowmodweb/site/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 
 # Wrap our patterns in /site/ to match our deployment environment
 urlpatterns = patterns('starbowmodweb.site.views',
-    url(r'', 'home', name='home')
+    url(r'^$', 'home', name='home')
 )
