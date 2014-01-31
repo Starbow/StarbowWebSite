dbd5ed71f7ebd767b19391c0b8966c23f93c332b
Graylin Kim <graylin.kim@gmail.com>
2014-01-31 13:05:34 -0500

    Remove old route I forgot to delete.

--- starbowmodweb/user/urls.py
+++ starbowmodweb/user/urls.py
@@ -4,7 +4,6 @@ from django.views.generic import TemplateView
 # Wrap our patterns in /site/ to match our deployment environment
 urlpatterns = patterns('starbowmodweb.user.views',
     url(r'^home', 'user_home', name='user_home'),
-    url(r'^activate', 'user_activate', name='user_activate'),
 
     # Registration and authorization paths
     url(r'^register', 'user_register', name='user_register'),
