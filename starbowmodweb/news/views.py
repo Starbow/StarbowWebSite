ecdde0ef68a295910039c0a3a4a26f0580fd81f6
Graylin Kim <graylin.kim@gmail.com>
2014-02-02 17:56:01 -0500

    Add support for the [img] tag.

--- starbowmodweb/news/views.py
+++ starbowmodweb/news/views.py
@@ -5,6 +5,17 @@ from django.db import connections
 import bbcode
 
 
+def bbcode_img(tag_name, value, options, parent, context):
+    if tag_name in options and 'x' in options[tag_name]:
+        options['width'], options['height'] = options[tag_name].split('x', 1)
+        del options[tag_name]
+    attrs = ' '.join([name+'="{}"' for name in options.keys()])
+    return ('<img src="{}" '+attrs+' />').format(value, *options.values())
+
+bbcode_parser = bbcode.Parser()
+bbcode_parser.add_formatter("img", bbcode_img, replace_links=False)
+
+
 def dictfetchall(cursor):
     "Returns all rows from a cursor as a dict"
     desc = cursor.description
@@ -19,6 +30,7 @@ def all_news(request):
     cursor.execute("SELECT * FROM mybb_threads JOIN mybb_posts WHERE firstpost=mybb_posts.pid AND mybb_threads.fid=%s AND mybb_threads.visible=1", [3])
     articles = dictfetchall(cursor)
     for article in articles:
-        article['html'] = bbcode.render_html(article['message'])
+
+        article['html'] = bbcode_parser.format(article['message'])
 
     return render(request, 'news_listing.html', dict(articles=articles))
