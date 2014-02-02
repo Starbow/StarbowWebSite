c5c86f3d313c5a765f5d34986409ca8e3ff1b1d7
Graylin Kim <graylin.kim@gmail.com>
2014-02-02 02:32:11 -0500

    Add horrible About page, someone should fix this up.

--- starbowmodweb/site/urls.py
+++ starbowmodweb/site/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 # Wrap our patterns in /site/ to match our deployment environment
 urlpatterns = patterns('starbowmodweb.site.views',
-    url(r'^$', 'home', name='home')
+    url(r'^$', 'home', name='home'),
+    url(r'^about', TemplateView.as_view(template_name='about.html')),
 )
