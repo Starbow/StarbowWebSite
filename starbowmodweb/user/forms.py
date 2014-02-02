caa6144f2c2ad779f02567d59c52a67a232f17c4
Graylin Kim <graylin.kim@gmail.com>
2014-02-02 01:45:47 -0500

    We don't need an activation form anymore.

--- starbowmodweb/user/forms.py
+++ starbowmodweb/user/forms.py
@@ -6,7 +6,3 @@ class RegistrationForm(forms.ModelForm):
     class Meta:
         model = auth.get_user_model()
         fields = ['username', 'email']
-
-
-class ActivationForm(forms.Form):
-    username = forms.CharField(max_length=100)
