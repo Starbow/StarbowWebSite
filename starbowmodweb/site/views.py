f91e95b5ff6e6e3cfdde3122890e87f2e09e81c8
Cezary Wojcik <cezarywojcik92@gmail.com>
2014-02-12 12:48:45 -0800

    about page update

--- starbowmodweb/site/views.py
+++ starbowmodweb/site/views.py
@@ -7,11 +7,11 @@ def home(request):
     now = datetime.now()
     later = now+timedelta(seconds=30*86400)
     upcoming_events = mybb.get_events_in_range("Default Calendar", now, later)[:7]
-    latest_article = mybb.get_threads(forum_name="News and Announcements", count=1, orderby="mybb_threads.dateline", sort="DESC")
+    latest_article = mybb.get_threads(forum_name="News and Announcements", count=1, orderby="mybb_threads.dateline", sort="DESC")[0]
     recent_articles = mybb.get_threads(forum_name="News and Announcements", count=7, offsetby=1, orderby="mybb_threads.dateline", sort="DESC")
     return render(request, 'site_home.html', dict(
         upcoming_events=upcoming_events,
-        latest_article=latest_article[0],
+        latest_article=latest_article,
         recent_articles=recent_articles
     ))
 
