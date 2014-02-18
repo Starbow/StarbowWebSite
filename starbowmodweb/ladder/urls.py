be75491a3e79b47a2501bbe00d6790ee72759792
Graylin Kim <graylin.kim@gmail.com>
2014-02-18 12:19:31 -0500

    Show EU ladder as well as NA ladder.

--- starbowmodweb/ladder/urls.py
+++ starbowmodweb/ladder/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 
 # Wrap our patterns in /site/ to match our deployment environment
 urlpatterns = patterns('starbowmodweb.ladder.views',
-    url(r'^$', 'show_ladder'),
+    url(r'^$', 'show_ladders'),
     url(r'map/([0-9]+)', 'show_map'),
     url(r'player/([0-9]+)', 'show_player'),
 )
--- starbowmodweb/ladder/views.py
+++ starbowmodweb/ladder/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from starbowmodweb.ladder.helpers import get_leaderboard
-from starbowmodweb.ladder.models import Client, Map, MatchResult, BATTLENET_REGION_NA
+from starbowmodweb.ladder.models import Client, Map, MatchResult, BATTLENET_REGION_NA, BATTLENET_REGION_EU
 
 
-def show_ladder(request):
+def show_ladders(request):
     ladder_na = get_leaderboard(region=BATTLENET_REGION_NA, orderby='ladder_points', sort="DESC", count=50)
-    return render(request, 'ladder_home.html', dict(ladder_stats=ladder_na))
+    ladder_eu = get_leaderboard(region=BATTLENET_REGION_EU, orderby='ladder_points', sort="DESC", count=50)
+    return render(request, 'ladder_home.html', dict(ladder_na=ladder_na, ladder_eu=ladder_eu))
 
 
 def show_player(request, client_id):
